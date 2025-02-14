d90595f1fd45d75c26fcdcb251a1e8124ddf0a78
Amiiir74a <amiiir74a@gmail.com>
2025-02-14 23:32:05 +0330

    Revert "29"
    This reverts commit 558b67b3b64ab4d957ca590faec46acc548fbb34.

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,6 @@ const CACHE_NAME = 'my-site-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/edit_workout.html',
   '/icon-192x192.png',
   // Add other assets you want to cache
 ];
@@ -11,12 +10,7 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        return fetch('b.json')
-          .then(response => response.json())
-          .then(data => {
-            const videoUrls = data.Sheet1.map(activity => activity.newurl).filter(url => url);
-            return cache.addAll([...urlsToCache, ...videoUrls]);
-          });
+        return cache.addAll(urlsToCache);
       })
   );
 });
